74debf54fa4199116c5108ada9b0a06bf698ad58
march101348 <32599831+march101348@users.noreply.github.com>
2022-08-20 20:21:21 +0900

    Feature/tauri demo 17 (#36)
    * 画像拡張子の追加
    
    * formatter適用

--- src/features/filepath/consts/images.ts
+++ src/features/filepath/consts/images.ts
@@ -1,1 +1,29 @@
-export const ImageExtensions = ['png', 'PNG', 'jpg', 'jpeg', 'JPG'];
+const JpegExtensions = [
+  'jpg',
+  'jpeg',
+  'JPG',
+  'JPEG',
+  'jpe',
+  'jfif',
+  'pjpeg',
+  'pjp',
+];
+
+const PngExtensions = ['png', 'PNG'];
+
+const GifExtensions = ['gif'];
+
+const TiffExtensions = ['tif', 'tiff'];
+
+const BmpExtensions = ['bmp', 'dib'];
+
+const WebpExtensions = ['webp'];
+
+export const ImageExtensions = [
+  ...JpegExtensions,
+  ...PngExtensions,
+  ...GifExtensions,
+  ...TiffExtensions,
+  ...BmpExtensions,
+  ...WebpExtensions,
+];
